3f72f8b6c8ab2ca49da5d94bb3c1b1ccc5cd694f
yjk <na86421@naver.com>
2021-02-03 21:45:33 +0900

    글쓴이 표시 기능 구현

--- pybo/views.py
+++ pybo/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.http import HttpResponse
+from .forms import QuestionForm, AnswerForm
 from .models import Question
 
 
@@ -30,36 +31,14 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    # Answer 모델이 Question 모델을 Foreign Key로 참조하고 있으므로 question.answer_set 표현을 사용할 수 있다.
-    # 하나의 질문에 여러개의 answer(답변)이 달릴 수 있음, 질문에 달린 답변은 set 을 통하여 답변 세트를 조회.
-    # Answer 모델을 직접 사용하여 데이터를 저장할 수도 있다.
-    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question_id)
-
-
-def question_create(request):
-    if request.method == 'POST':
-        form = QuestionForm(request.POST)
-        if form.is_valid():
-            question = form.save(commit=False)  # commit=False -> 임시저장
-            question.create_date = timezone.now()
-            question.save()
-            return redirect('pybo:index')
-    else:
-        form = QuestionForm()
-    context = {'form': form}
-    return render(request, 'pybo/question_form.html', context)
-
-
-def answer_create(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # 답변 글쓴이는 현재 로그인한 계정
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -68,3 +47,25 @@ def answer_create(request, question_id):
         form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
+
+    # Answer 모델이 Question 모델을 Foreign Key로 참조하고 있으므로 question.answer_set 표현을 사용할 수 있다.
+    # 하나의 질문에 여러개의 answer(답변)이 달릴 수 있음, 질문에 달린 답변은 set 을 통하여 답변 세트를 조회.
+    # Answer 모델을 직접 사용하여 데이터를 저장할 수도 있다.
+    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+
+
+@login_required(login_url='common:login')
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)  # commit=False -> 임시저장
+            question.author = request.user
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
