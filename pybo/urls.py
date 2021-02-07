b27a48486e945900a4a5b8b5fa6816f2bda0eda0
yjk <na86421@naver.com>
2021-02-07 17:38:04 +0900

    추천 기능 구현

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import base_views, question_views, answer_views, comment_views
+from .views import base_views, question_views, answer_views, comment_views, vote_views
 
 from . import views
 
@@ -8,7 +8,7 @@ app_name = 'pybo'
 
 urlpatterns = [
     # base_views.py
-    path('', views.index, name='index'),
+    path('', base_views.index, name='index'),
     path('<int:question_id>/', base_views.detail, name='detail'),
 
     # question_views.py
@@ -28,4 +28,8 @@ urlpatterns = [
     path('comment/create/answer/<int:answer_id>/', comment_views.comment_create_answer, name='comment_create_answer'),
     path('comment/modify/answer/<int:comment_id>/', comment_views.comment_modify_answer, name='comment_modify_answer'),
     path('comment/delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
+
+    # vote_views.py
+    path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+    path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
 ]
