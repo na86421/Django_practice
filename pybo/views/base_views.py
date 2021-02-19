3983139521b9cc58212911d65044935cc78bde36
yjk <na86421@naver.com>
2021-02-19 21:57:07 +0900

    스크롤 초기화 문제점 해결, 마크다운 기능, 검색 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
@@ -7,15 +8,23 @@ from ..models import Question
 def index(request):
     # 입력 파라미터
     page = request.GET.get('page', 1)  # default page 1
+    kw = request.GET.get('kw', '')  # 검색어
 
     # 작성 일시 역순으로 데이터 조회
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
 
     # pagination
     paginator = Paginator(question_list, 10)  # per page 10
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 
