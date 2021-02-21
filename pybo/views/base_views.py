fa5da931d50003f577ea092d9cb3c3ec657bfb3a
yjk <na86421@naver.com>
2021-02-21 13:32:41 +0900

    정렬 기능 구현

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from ..models import Question
 
@@ -9,9 +9,16 @@ def index(request):
     # 입력 파라미터
     page = request.GET.get('page', 1)  # default page 1
     kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬기준
 
-    # 작성 일시 역순으로 데이터 조회
-    question_list = Question.objects.order_by('-create_date')
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |  # 제목검색
@@ -24,7 +31,7 @@ def index(request):
     paginator = Paginator(question_list, 10)  # per page 10
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so':so}
     return render(request, 'pybo/question_list.html', context)
 
 
